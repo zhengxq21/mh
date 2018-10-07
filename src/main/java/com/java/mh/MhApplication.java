8066ad8cbba131eaa7e08bbda8878c2f8f2bc7cb
zhengxq21 <675942639@qq.com>
2018-10-08 01:04:50 +0800

    first commit

---
+++ src/main/java/com/java/mh/MhApplication.java
@@ -0,0 +1,12 @@
+package com.java.mh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhApplication.class, args);
+    }
+}
