f58dc567d53686d49870c23d6f8e881f7d7b219c
zhengxq21 <675942639@qq.com>
2018-10-16 23:44:30 +0800

    添加电影页面更新

--- src/main/webapp/static/admin/addCartoon.js
+++ src/main/webapp/static/admin/addCartoon.js
@@ -6,6 +6,13 @@ $(function(){
         data:{},
         dataType:'json',
         success:function(result){
+            var typeHtml = "";
+            if (result!= '' && result != null){
+                for (var i=0;i<result.length;i++){
+                    typeHtml += "<input type='checkbox' id='type' name='type' value='"+result[i].id+"'>"+result[i].typeName+"&nbsp;";
+                }
+                $("#typeCheckBox").html(typeHtml);
+            }
 
         }
     })
@@ -17,6 +24,13 @@ $(function(){
         data:{},
         dataType:'json',
         success:function(result){
+            if (result != '' && result != null){
+                var districtHtml = "";
+                for (var i = 0;i < result.length;i++){
+                    districtHtml += "<input type='radio' name='district' id='district' value='"+result[i].id+"'>"+result[i].districtName+"&nbsp;";
+                }
+                $("#districtCheckBox").html(districtHtml);
+            }
         }
     })
 
