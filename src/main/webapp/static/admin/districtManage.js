584fe8975d400a30897135e05ca9f88ba3cbedf0
zhengxq21 <675942639@qq.com>
2018-10-25 23:43:24 +0800

    漫画发行地区功能更新

--- src/main/webapp/static/admin/districtManage.js
+++ src/main/webapp/static/admin/districtManage.js
@@ -19,3 +19,56 @@ function openDistrictModifyDialog(){
     $("#dlg").dialog("open").dialog("setTitle","编辑漫画地区信息");
     url = "/admin/district/save?id"+rows.id;
 }
+
+function deleteDistrict(){
+    var selectedRows = $("#dg").datagrid("getSelections");
+    if (selectedRows.length == 0){
+        $.messager.alert("系统提示","请选择要删除的数据");
+    }
+    var strIds=[];
+    for (var i=0;i<selectedRows.length;i++){
+        strIds.push(selectedRows[i].id);
+    }
+    var ids = strIds.join(",");
+    $.messager.confirm("系统提示","您确定要删除这<font color='red'>"+selectedRows.length+"</font>条数据吗？"
+    ,function(r){
+        $.post("/admin/district/delete",{'ids':ids},function(result){
+            if (result.success){
+                $.messager.alert("系统提示","数据已删除成功！");
+                $("#dg").datagrid("reload");
+            }else{
+                $.messager.alert("系统提示","数据删除失败，请联系管理员");
+            }
+        },"json");
+        });
+}
+
+
+function saveDistrict(){
+    $("#fm").form("submit",{
+       url:url,
+       onSubmit:function(){
+            return $(this).form("validate");
+       },
+       success:function(result){
+           var result = eval('('+result+')');
+           if (result.success){
+               $.messager.alert("系统提示","保存成功！");
+               resetValue();
+               $("#dlg").dialog("close");
+               $("#dg").datagrid("reload");
+           }
+       }
+    });
+}
+
+
+function resetValue(){
+    $("#districtName").val("");
+}
+
+
+function closeDistrictDialog(){
+    resetValue();
+    $("#dlg").dialog("close");
+}
