0ba9523eae0e7006d0b9e9dc6d05c9aa4c38d732
zhengxq21 <675942639@qq.com>
2018-10-30 22:29:08 +0800

    漫画列表service

--- src/main/java/com/java/mh/service/impl/CartoonServiceImpl.java
+++ src/main/java/com/java/mh/service/impl/CartoonServiceImpl.java
@@ -3,9 +3,18 @@ package com.java.mh.service.impl;
 import com.java.mh.entity.Cartoon;
 import com.java.mh.repository.CartoonRepository;
 import com.java.mh.service.CartoonService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Service("cartoonService")
@@ -21,7 +30,16 @@ public class CartoonServiceImpl implements CartoonService {
 
     @Override
     public List<Cartoon> list(Cartoon cartoon, Integer page, Integer rows) {
-        return null;
+        Pageable pageable = new PageRequest(page,rows,Sort.Direction.DESC);
+        Page<Cartoon> cartoons = cartoonRepository.findAll(new Specification<Cartoon>(){
+
+            @Override
+            public Predicate toPredicate(Root<Cartoon> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return null;
+            }
+        },pageable);
+        }
+        retur;
     }
 
     @Override
